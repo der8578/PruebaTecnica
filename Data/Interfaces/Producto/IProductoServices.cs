126e7bc3bda233fe4434603b9e2b4bb91ffa40fb
Derick Jose Espinoza Roche <despinoza@MacBook-Pro-de-Derick.local>
2025-06-11 12:41:29 -0600

    fix IProductoServices

--- Data/Interfaces/Producto/IProductoServices.cs
+++ Data/Interfaces/Producto/IProductoServices.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Models.Producto;
 
 namespace Data.Interfaces.Producto
 {
     public interface IProductoServices
     {
-        
+        Task<IEnumerable<ProductoModel>> ObtenerProductos(string searchTerm);
     }
 }
